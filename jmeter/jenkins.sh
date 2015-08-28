bc90704066fab3134bea0e78dcbf74558687c29e
jackchem2003 <jackchem2003@gmail.com>
2015-08-28 03:12:43 -0700

    Update jenkins.sh

--- jmeter/jenkins.sh
+++ jmeter/jenkins.sh
@@ -1,1 +1,11 @@
+#!/bin/bash
+echo "### Running JMeter performance test ###"
 
+# Clear out old results
+rm $WORKSPACE/jmeter.jtl
+
+# Run the tests
+echo "## Running the tests"
+cd "$WORKSPACE/jmeter"
+
+jmeter -n -t test_plan.jmx -l $WORKSPACE/jmeter.jtl -p user.properties
